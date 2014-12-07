07cc5d03e832b8a2c7e475baecd5928b229845ec
Alexander Mogilka <alexander.mogilka@creuna.com>
2014-12-07 14:46:01 +0200

    simplified server implementation

--- NetworkFileTransfer.Server/ServerHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../NetworkFileTransfer.Core/NetworkHelper.h"
-
-class ServerHelper : public NetworkHelper
-{
-protected:
-	virtual BOOL initSocket(int socketId, const addrinfo* address);
-};
--- NetworkFileTransfer.Server/main.cpp
+++ NetworkFileTransfer.Server/main.cpp
@@ -1,11 +1,155 @@
+#pragma once
 #include <stdio.h>
-#include "Server.h"
+#include <WS2tcpip.h>
+#include <windows.h>
+
+const char* host = "127.0.0.1";
+const char* port = "33444";
+const int queueSize = 10;
+
+void WSAInitialize();
+void printError(const char* message);
+SOCKET getSocket(const char* host, const char* port);
+void run(SOCKET serverSocket);
 
 int main()
 {
-	Server server("127.0.0.1", "33444", 10);
-	server.run();
+	WSAInitialize();
+
+	SOCKET serverSocket = getSocket(host, port);
+	if (serverSocket == -1)
+	{
+		printError("can't run server");
+		return 1;
+	}
+
+	if (listen(serverSocket, queueSize) == -1)
+	{
+		printError("listen");
+		return 1;
+	}
+
+	run(serverSocket);
+
+	closesocket(serverSocket);
+
+	WSACleanup();
 
 	system("pause");
 	return 0;
+}
+
+void printError(const char* message)
+{
+	fprintf(stderr, "Error: %s\n", message);
+}
+
+void WSAInitialize()
+{
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+	{
+		printError("WSAStartup failed");
+		exit(1);
+	}
+}
+
+SOCKET getSocket(const char* hostname, const char* port)
+{
+	int socketId;
+	addrinfo *address, hints;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	int result;
+	if ((result = getaddrinfo(hostname, port, &hints, &address)) != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result));
+		return -1;
+	}
+
+	addrinfo *p;
+	for (p = address; p != NULL; p = p->ai_next)
+	{
+		if ((socketId = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+		{
+			printError("getSocket.socket");
+			continue;
+		}
+
+		char optValue = 1;
+		if (setsockopt(socketId, SOL_SOCKET, SO_REUSEADDR, &optValue, sizeof(char)) == -1)
+		{
+			printError("setsockopt");
+			continue;
+		}
+
+		if (bind(socketId, address->ai_addr, address->ai_addrlen) == -1)
+		{
+			closesocket(socketId);
+			printError("bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL)
+	{
+		printError("getSocket");
+		return -1;
+	}
+
+	freeaddrinfo(address);
+
+	return socketId;
+}
+
+void run(SOCKET serverSocket)
+{
+	SOCKET socket;
+	sockaddr_storage address;
+	socklen_t addressSize;
+
+	int recvSize;
+	const int bufferSize = 255;
+	char buffer[bufferSize];
+	char saddress[INET6_ADDRSTRLEN];
+
+	printf("server: waiting for connections...\n");
+
+	while (1)
+	{
+		addressSize = sizeof(address);
+		socket = accept(serverSocket, (sockaddr*)&address, &addressSize);
+		if (socket == -1)
+		{
+			printError("accept");
+			continue;
+		}
+
+		/*inet_ntop(address.ss_family, convertSocketAddress((sockaddr*)&address), saddress, sizeof(saddress));
+		printf("server: got connection from %s\n", saddress);*/
+
+		while ((recvSize = recv(socket, buffer, bufferSize - 1, 0)) != -1)
+		{
+			buffer[recvSize] = '\0';
+
+			printf("Received from client: %s\n", buffer);
+
+			if (send(socket, buffer, recvSize, 0) == -1)
+			{
+				printError("send");
+			}
+
+			if (strcmp(buffer, "end") == 0)
+			{
+				printf("Closing connection with client: %s\n", saddress);
+				closesocket(socket);
+				break;
+			}
+		}
+	}
 }
